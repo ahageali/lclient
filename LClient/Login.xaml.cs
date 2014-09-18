7e4fcc85c4d588664363af9ac774b97d8463ac8e
Abdul-Monhem Hage-ali <abdul.hageali@gmail.com>
2014-09-18 16:33:06 -0400

    done working on http-end; time to work on RTMP stuff

--- LClient/Login.xaml.cs
+++ LClient/Login.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using MahApps.Metro.Controls;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using LClient.Supporting.Models.HTTP;
+using MahApps.Metro.Controls.Dialogs;
 
 namespace LClient
 {
@@ -25,9 +29,56 @@ namespace LClient
             InitializeComponent();
         }
 
-        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
         {
+            if (!(string.IsNullOrEmpty(txtUser.Text) || string.IsNullOrEmpty(txtPass.Password)))
+            {
+                var progressController = await DialogManager.ShowProgressAsync(this, "Logging in", "Contacting HTTP Server");
+                WebRequest req = WebRequest.Create("https://lq.na1.lol.riotgames.com/login-queue/rest/queues/lol/authenticate");
+                req.ContentType = "application/x-www-form-urlencoded";
+                req.Method = "POST";
+                req.UseDefaultCredentials = true;
+                using (StreamWriter writer = new StreamWriter(await req.GetRequestStreamAsync()))
+                {
+                    await writer.WriteAsync("payload=user=" + txtUser.Text + ",password=" + txtPass.Password);
+                }
+                WebResponse resp = null;
+                try
+                {
+                    resp = await req.GetResponseAsync();
+                    using (StreamReader reader = new StreamReader(resp.GetResponseStream()))
+                    {
+                        string strRes = await reader.ReadToEndAsync();
+                        HTTPLoginResult res = await Task.Factory.StartNew(() => Newtonsoft.Json.JsonConvert.DeserializeObject<HTTPLoginResult>(strRes));
+                        switch (res.status)
+                        {
+                            case "LOGIN":
+                                progressController.SetMessage("Contacting RTMP Server");
 
+                                break;
+                            case "QUEUE":
+                                //TODO finish this
+                                break;
+                            default:
+                                await ProgressFailed(progressController);
+                                break;
+                        }
+                    }
+                    return;
+                }
+                catch (Exception excep)
+                {
+                }
+                //TODO work on this to specify reason
+                await ProgressFailed(progressController);
+            }
+        }
+
+        private async Task ProgressFailed(ProgressDialogController controller)
+        {
+            controller.SetMessage("Failed");
+            await Task.Delay(1500);
+            await controller.CloseAsync();
         }
     }
 }
--- LClient/Supporting/LRtmp.cs
+++ LClient/Supporting/LRtmp.cs
@@ -10,10 +10,16 @@ namespace LClient.Supporting
     class LRtmp
     {
         RtmpSharp.Net.RtmpClient mainClient;
+        public String SummonerName {get; set;}
 
         public LRtmp()
         {
+        }
 
+        public async Task<bool> Login(com.riotgames.platform.login.Session session)
+        {
+
+            return false;
         }
     }
 }
